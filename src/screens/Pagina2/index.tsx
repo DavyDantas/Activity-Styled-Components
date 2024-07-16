dbf9abccb3467821d69a0d9a484e2958b4380f73
Davy Dantas <davy.dantas@escolar.ifrn.edu.br>
2024-07-16 13:06:04 -0300

    first

---
+++ src/screens/Pagina2/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, StyledText, StyledButton, ButtonText, Input, StyledPicker } from './styles';
+import { Picker } from '@react-native-picker/picker';
+
+export default function App() {
+  return (
+    <Container>
+      <StyledText>Cadastro de Tarefa</StyledText>
+      <Input placeholder="Título" />
+      <Input placeholder="Descrição" />
+      <StyledPicker>
+        <Picker.Item label="Opção 1" value="option1" />
+        <Picker.Item label="Opção 2" value="option2" />
+        <Picker.Item label="Opção 3" value="option3" />
+      </StyledPicker>
+      <Input placeholder="Data" />  
+      <StyledButton onPress={() => alert('Botão pressionado!')}>
+        <ButtonText>Cadastrar</ButtonText>
+      </StyledButton>
+    </Container>
+  );
+}
---
+++ src/screens/Pagina2/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components/native";
+import { Picker } from '@react-native-picker/picker';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: white;
+`;
+
+export const StyledText = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 101px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  background-color: ${({theme}) => theme.colors.primary};
+  border-radius: 5px;
+  margin-top: 20px;
+  width: 80%;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+`;
+
+export const Input = styled.TextInput`
+    width: 80%;
+    height: 60px;
+    background-color: transparent;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin-bottom: 20px;
+    font-size: 18px;
+    font-weight: bold;
+    border: 1px ${({ theme }) => theme.colors.gray};
+    font-style: italic;
+    `;
+
+export const StyledPicker = styled(Picker)`
+    width: 80%;
+    height: 60px;
+    background-color: transparent;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin-bottom: 20px;
+    font-size: 18px;
+    font-weight: bold;
+    border: 1px ${({ theme }) => theme.colors.gray};
+    font-style: italic;
+`;
